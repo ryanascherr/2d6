8f7dd67445544d344fb52a1f2c7023f5a0233302
ryanascherr <ryanascherr@gmail.com>
2022-01-30 12:41:36 -0500

    Removed unnecessary comments.

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-// const cubeOne = $(".cube-one");
-// const cubeTwo = $(".cube-two");
-
 let isDiceOneLow = true;
 let isDiceTwoLow = true;
 let isDiceRolling = false;
 let roll = 0;
-// let roll1 = 0;
-// let roll2 = 0;
 let rollTime = 750;
 let modifier = 0;
 let numberOfDice = 1;
 
 displayInitialDiceNumber();
+displayInitialDiceStyle();
+displayInitialModifier();
 
 function displayInitialDiceNumber() {
     let numberOfDice = localStorage.getItem("number-of-dice");
@@ -24,18 +21,6 @@ function displayInitialDiceNumber() {
     }
     $(`.number-of-dice option[value=${numberOfDice}]`).attr('selected', '');
 };
-
-$(".number-of-dice").change(function(){
-    $(".cube").remove();
-    localStorage.setItem("number-of-dice", this.value);
-    let newDiceNumber = parseInt(this.value);
-    for (let i = 0; i < newDiceNumber; i++) {
-        $('.container').append('<div class="cube face-2-low low"><div class="face front">1</div><div class="face back">6</div><div class="face bottom">2</div><div class="face left">3</div><div class="face right">4</div><div class="face top">5</div></div>');
-    }
-    displayInitialDiceStyle();
-});
-
-displayInitialDiceStyle();
 
 function displayInitialDiceStyle() {
     let style = localStorage.getItem("dice-style");
@@ -78,12 +63,32 @@ function displayInitialDiceStyle() {
     $(".face").css('color', numberColor);
 }
 
+function displayInitialModifier() {
+    let modifierFromStorage = parseInt(localStorage.getItem("modifier"));
+    if (!modifierFromStorage) {
+        modifierFromStorage = 0;
+        localStorage.setItem("modifier", modifierFromStorage);
+    }
+    modifier = modifierFromStorage;
+    $(`.modifier option[value=${modifierFromStorage}]`).attr('selected', '');
+};
+
+$(".number-of-dice").change(function(){
+    $(".cube").remove();
+    localStorage.setItem("number-of-dice", this.value);
+    let newDiceNumber = parseInt(this.value);
+    for (let i = 0; i < newDiceNumber; i++) {
+        $('.container').append('<div class="cube face-2-low low"><div class="face front">1</div><div class="face back">6</div><div class="face bottom">2</div><div class="face left">3</div><div class="face right">4</div><div class="face top">5</div></div>');
+    }
+    displayInitialDiceStyle();
+});
+
 $(".style").change(function(){
     localStorage.setItem("dice-style", this.value);
     let style = this.value;
     let backgroundColor = '';
     let borderColor = '';
-    let numberColor = ''
+    let numberColor = '';
 
     if (style === "classic") {
         backgroundColor = "black";
@@ -115,18 +120,6 @@ $(".style").change(function(){
     $(".face").css('color', numberColor);
 });
 
-displayInitialModifier();
-
-function displayInitialModifier() {
-    let modifierFromStorage = parseInt(localStorage.getItem("modifier"));
-    if (!modifierFromStorage) {
-        modifierFromStorage = 0;
-        localStorage.setItem("modifier", modifierFromStorage);
-    }
-    modifier = modifierFromStorage;
-    $(`.modifier option[value=${modifierFromStorage}]`).attr('selected', '');
-};
-
 $(".modifier").change(function(){
     let newModifier = parseInt(this.value);
     localStorage.setItem("modifier", this.value);
@@ -134,7 +127,6 @@ $(".modifier").change(function(){
 });
 
 $(".roll-btn").click(function() {
-    //Does not allow user to roll while a roll is currently happening
     if (isDiceRolling) {
         return;
     }
@@ -145,22 +137,9 @@ $(".roll-btn").click(function() {
         isDiceRolling = false;
     }, rollTime);
 
-    // roll1 = Math.floor(Math.random() * 6 + 1);
-    // roll2 = Math.floor(Math.random() * 6 + 1);
-
-    // resetClasses();
     rollAllDice();
-    // rollDiceOne();
-    // rollDiceTwo();
     displayResult();
 })
-
-// function resetClasses() {
-//     $(cubeOne).removeClass();
-//     $(cubeOne).addClass("cube cube-one");
-//     $(cubeTwo).removeClass();
-//     $(cubeTwo).addClass("cube cube-two");
-// }
 
 function rollAllDice() {
     roll = 0;
@@ -184,42 +163,12 @@ function rollAllDice() {
     });
 }
 
-// function rollDiceOne() {
-//     roll1 = Math.floor(Math.random() * 6 + 1);
-
-//     let numberAsString = JSON.stringify(roll1);
-
-//     if (isDiceOneLow) {
-//         $(cubeOne).addClass(`face-${numberAsString}-high`);
-//         isDiceOneLow = false;
-//         return;
-//     }
-
-//     $(cubeOne).addClass(`face-${numberAsString}-low`);
-//     isDiceOneLow = true;
-// }
-
-// function rollDiceTwo() {
-//     let numberAsString = JSON.stringify(roll2);
-
-//     if (isDiceTwoLow) {
-//         $(cubeTwo).addClass(`face-${numberAsString}-high`);
-//         isDiceTwoLow = false;
-//         return;
-//     }
-
-//     $(cubeTwo).addClass(`face-${numberAsString}-low`);
-//     isDiceTwoLow = true;
-// }
-
 function displayResult() {
     const diceRollTotal = $(".total");
     setTimeout(function(){
       if (modifier == 0) {
-        //   diceRollTotal.text(roll1 + roll2);
           diceRollTotal.text(roll);
       } else {
-        //   diceRollTotal.text(`${roll1 + roll2} + ${modifier} = ${roll1 + roll2 + modifier}`);
           diceRollTotal.text(`${roll} + ${modifier} = ${roll + modifier}`);
       }
     }, rollTime);
